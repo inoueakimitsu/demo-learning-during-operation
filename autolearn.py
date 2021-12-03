f364a9e05370bb36101fe17d1e7dee6d4d4462cb
Akimitsu Inoue <akimitsu.inoue@gmail.com>
2021-12-03 10:05:41 +0900

    first commit

---
+++ autolearn.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+from sklearn.neighbors import KNeighborsRegressor
+
+import matplotlib.pyplot as plt
+import matplotlib
+plt.style.use('ggplot')
+font = {'family': 'meiryo'}
+matplotlib.rc('font', **font)
+
+import streamlit as st
+from streamlit_autorefresh import st_autorefresh
+
+def true_data(t):
+    return np.sin(t/7 + np.cos(t/5.2)) + np.random.normal(loc=0, scale=0.1, size=len(t) if not isinstance(t, int) else 1)
+
+if 'ts' not in st.session_state or 'ys' not in st.session_state:
+    st.session_state['ts'] = np.arange(-200, 0)
+    st.session_state['ys'] = true_data(st.session_state['ts']).tolist()
+    st.session_state['ts'] = st.session_state['ts'].tolist()
+
+st.title('Demo: Learning during operation')
+
+'''This is a demonstration of a time series forecasting model that continuously learns while in operation.'''
+
+'''Learning is performed approximately once per second.
+The learning process uses the K nearest neighbor method, which has a small computational load.'''
+
+new_t = st_autorefresh(interval=1000, limit=1000, key="data_input")
+new_y = true_data(new_t)[0]
+
+st.session_state['ts'].append(new_t)
+st.session_state['ys'].append(new_y)
+
+window = 10
+horizon = 20
+
+X = []
+Y = []
+for i in range(len(st.session_state['ts'])-window-horizon):
+    X.append(st.session_state['ys'][i:i+window])
+    Y.append(st.session_state['ys'][i+horizon])
+X = np.array(X)
+Y = np.array(Y)
+
+model = KNeighborsRegressor(n_neighbors=1)
+model.fit(X, Y)
+
+pred_ys = st.session_state['ys'][-window:]
+pred_ts = st.session_state['ts'][-window:]
+for i in range(horizon):
+    pred_ys.append(model.predict(
+        [pred_ys[-window:]]
+    )[0])
+    pred_ts.append(new_t + i)
+
+fig = plt.figure(figsize=(12, 4))
+ax = plt.axes()
+plt.plot(st.session_state['ts'], st.session_state['ys'], label="Observed")
+plt.plot(pred_ts, pred_ys, "--", label="Predicted")
+plt.ylabel("value")
+plt.xlabel("time")
+plt.legend()
+
+st.pyplot(fig)
